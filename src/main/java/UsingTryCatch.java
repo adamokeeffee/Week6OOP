b399023ff626a7e5889593bc0e2fd577cd3cc523
ADAMOKEEFFE-STUDENT <g00434897@atu.ie>
2024-10-23 17:46:34 +0100

    try with resources

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -1,35 +1,35 @@
+import org.xml.sax.InputSource;
+
 import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
 
-        Scanner myscan = new Scanner(System.in);
 
+        try (Scanner myscan = new Scanner(System.in)) {
+            while (true) {
+                try {
 
-       while(true) {
-           try {
-               System.out.println("Please enter a number between 0  -  9 :");
+                    System.out.println("Please enter a number between 0  -  9 :");
 
-               // Read user input as a sting
-               String input = myscan.nextLine();
+                    // Read user input as a sting
+                    String input = myscan.nextLine();
 
-               // Parse the input to an integer
-               int myNum = Integer.parseInt(input);
+                    // Parse the input to an integer
+                    int myNum = Integer.parseInt(input);
 
-               // Check if the number is between 0 and 9
-               if (myNum >= 0 && myNum <= 9) {
-                   System.out.println("You entered " + myNum);
-                   break;
-               } else {
-                   System.out.println("Not a valid number please enter a number within the number range");
-               }
-           } catch (NumberFormatException e) {
-               System.out.println("Please enter a valid NUMBER");
-               }
-           finally {
-               myscan.close();
-           }
+                    // Check if the number is between 0 and 9
+                    if (myNum >= 0 && myNum <= 9) {
+                        System.out.println("You entered " + myNum);
+                        break;
+                    } else {
+                        System.out.println("Not a valid number please enter a number within the number range");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a valid NUMBER");
+                }
 
-       }
+            }
+        }
     }
 }
